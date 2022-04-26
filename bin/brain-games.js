883de03694ddee6803b7a83072453fa9f019c634
Pavel Harelik <pasha.harelik@gmail.com>
2022-04-26 17:51:00 +0300

    Change function name

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
 
-import { greet } from '../src/cli.js';
+import { greetUser } from '../src/cli.js';
 
-greet();
+greetUser();
--- src/cli.js
+++ src/cli.js
@@ -1,9 +1,9 @@
 import readLineSync from 'readline-sync';
 
-const greet = () => {
+const greetUser = () => {
   console.log('Welcome to the Brain Games!');
   const name = readLineSync.question('May I have your name? ');
   console.log(`Hello, ${name}!`);
 };
 
-export { greet };
+export { greetUser };
