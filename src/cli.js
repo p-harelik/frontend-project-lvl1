e55d7e3b67ff557306750ecdd80a0c58a62950f4
Pavel Harelik <pasha.harelik@gmail.com>
2022-04-26 17:48:13 +0300

    Revert "Update call and name function"
    This reverts commit e695584e4edc2f6668c1ee0408c44c9802307022.

--- src/cli.js
+++ src/cli.js
@@ -1,9 +1,9 @@
 import readLineSync from 'readline-sync';
 
-const greetUSer = () => {
+const greet = () => {
   console.log('Welcome to the Brain Games!');
   const name = readLineSync.question('May I have your name? ');
   console.log(`Hello, ${name}!`);
 };
 
-export { greetUser };
+export { greet };
