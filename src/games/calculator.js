ad7f3f249829de098e16efe8c4e54d7c0b2ee87f
Pavel Harelik <pasha.harelik@gmail.com>
2022-09-20 12:41:04 +0300

    Add game brain-calc

---
+++ src/games/calculator.js
@@ -0,0 +1,31 @@
+import getRandomNumber from '../get-random.js';
+import runCommonLogic from '../index.js';
+
+const description = 'What is the result of the expression?';
+
+const calculate = (number1, number2, operator) => {
+  switch (operator) {
+    case '+':
+      return number1 + number2;
+    case '-':
+      return number1 - number2;
+    case '*':
+      return number1 * number2;
+    default:
+      throw new Error(`Unknown operator: ${operator}`);
+  }
+};
+
+const buildRound = () => {
+  const operators = ['+', '-', '*'];
+  const number1 = getRandomNumber(1, 100);
+  const number2 = getRandomNumber(1, 100);
+  const operator = operators[getRandomNumber(0, operators.length - 1)];
+  const question = `${number1}${operator}${number2}`;
+  const correctAnswer = String(calculate(number1,number2,operator));
+  return [question, correctAnswer];
+};
+
+export default () => {
+  runCommonLogic(description, buildRound);
+};
